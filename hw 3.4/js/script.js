2eb3e72f9d123619347a26af59fdc6d12cfde90f
1Djeka <41345334+1Djeka@users.noreply.github.com>
2018-09-04 23:41:09 +0300

    hw 3.4

---
+++ hw 3.4/js/script.js
@@ -0,0 +1,8 @@
+console.log('You are at' +window.location);
+var a = prompt(' your money')
+var a1 = prompt('purchased apples')
+var a2 = prompt('purchased bread')
+var a3 = prompt('cost apple')*3
+var a4 = prompt('cost bread')*2
+var a5 = a>a3+a4
+document.body.innerHTML = a5 = a>a3+a4
---
+++ practica2.js.js
@@ -0,0 +1,7 @@
+var a = prompt(' your money')
+var a1 = prompt('purchased apples')
+var a2 = prompt('purchased bread')
+var a3 = prompt('cost apple')*3
+var a4 = prompt('cost bread')*2
+var a5 = a>a3+a4
+document.body.innerHTML = a5 = a>a3+a4
